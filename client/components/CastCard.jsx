e0b96f3f6719f10043ab97b4494bb142a67faca0
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-06-07 13:52:38 +0100

    cast card

---
+++ client/components/CastCard.jsx
@@ -0,0 +1,13 @@
+const CastCard = () => {
+  return (
+    <div className="flex flex-col gap-3">
+      <img
+        src="https://s.hdnux.com/photos/10/12/44/2140469/11/1200x0.jpg"
+        alt="Tony Soprano"
+        className="w-28 max-w-full h-auto rounded-md"
+      />
+      <h3>Tony Soprano</h3>
+    </div>
+  );
+};
+export default CastCard;
