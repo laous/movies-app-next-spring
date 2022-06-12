04e55c18fd95fcc1c961178406cd121d27752f83
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-06-12 15:16:46 +0100

    show movies details in card

--- client/components/home/HomeMoviesListItem.jsx
+++ client/components/home/HomeMoviesListItem.jsx
@@ -1,25 +1,29 @@
 import Link from "next/link";
-
+import { imageUrl } from "../../constants/";
 const HomeMoviesListItem = ({ movie }) => {
   return (
-    <div className="flex flex-col space-y-2 my-3 max-w-[47%] md:max-w-none">
-      <div className="relative group w-64 max-w-full h-auto">
+    <div className="flex flex-col  space-y-2 my-3 max-w-[47%]  md:max-w-[36%] lg:max-w-[22%]">
+      <div className="relative group w-64 h-auto">
         {/* Image */}
         <img
-          className="w-64 max-w-full h-auto group-hover:opacity-25"
-          src="https://m.media-amazon.com/images/I/71-WBN3FCBL._AC_SL1280_.jpg"
+          className="w-64 max-w-full h-auto group-hover:opacity-25 rounded-xl"
+          src={imageUrl + movie?.poster_path}
         />
         {/* Content on hover */}
-        <div className="max-w-full absolute bg-black bottom-0 top-0 left-0 right-0 opacity-0 group-hover:opacity-70 flex flex-col justify-between items-center py-5 px-4">
+        <div className="max-w-full absolute bg-black bottom-0 top-0 left-0 right-0 opacity-0 group-hover:opacity-80 flex flex-col justify-between items-center py-5 px-4">
           <div className="flex flex-col gap-4">
             <header className="flex flex-col gap-1">
-              <h2 className="text-xl md:text-2xl font-semibold">
+              <h2 className="text-lg md:text-xl font-semibold">
                 {movie ? movie.title : "Your Name"}
               </h2>
               <div className="flex items-center justify-between">
-                <span className="text-sm md:text-base font-medium">+16</span>
+                <span className="text-sm md:text-base font-medium">
+                  {movie?.release_date.split("-")[0]}
+                </span>
                 <span className="text-base font-medium">|</span>
-                <span className="text-base font-medium">+16</span>
+                <span className="text-base font-medium">
+                  {movie?.original_language.toUpperCase()}
+                </span>
                 <span className="text-base font-medium">|</span>
                 <span className="text-base font-medium">+16</span>
               </div>
@@ -41,9 +45,8 @@ const HomeMoviesListItem = ({ movie }) => {
             <div className="flex flex-col gap-1">
               <h4 className="text-base md:text-lg font-semibold">Story</h4>
               <p className="text-xs md:text-sm font-normal">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-                eum voluptates iste officiis obcaecati animi nemo explicabo
-                illum.
+                {movie?.overview.slice(0, 200) +
+                  (movie?.overview.length > 200 ? " ..." : "")}
               </p>
             </div>
           </div>
@@ -57,7 +60,7 @@ const HomeMoviesListItem = ({ movie }) => {
 
       <div className="flex items-center space-x-5 justify-between px-1">
         <div className="flex flex-col">
-          <h3 className="text-xl">Your Name</h3>
+          <h3 className="text-base">{movie?.title}</h3>
           <span className="text-xs">By Makoto Shinkai</span>
         </div>
         <Link href={"/"}>
