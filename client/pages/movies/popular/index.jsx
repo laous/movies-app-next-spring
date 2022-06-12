7e9b83f2d1c13c2d9c4af9e1547f356691d94377
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-06-12 14:56:43 +0100

    display popular movies page

--- client/pages/movies/popular/index.jsx
+++ client/pages/movies/popular/index.jsx
@@ -1,30 +1,33 @@
+import axios from "axios";
 import { HomeMoviesListItem } from "../../../components";
 
-const Popular = () => {
+const Popular = ({ movies }) => {
   return (
     <section className=" flex flex-col items-center justify-center space-y-5 m-5">
       <header className="w-full max-w-4xl flex items-center justify-between">
         <h2 className="text-xl">Popular Movies</h2>
       </header>
       <div className="flex flex-wrap justify-center items-stretch gap-4">
-        <HomeMoviesListItem />
-        <HomeMoviesListItem />
-        <HomeMoviesListItem />
-        <HomeMoviesListItem />
-        <HomeMoviesListItem />
-        <HomeMoviesListItem />
-        <HomeMoviesListItem />
-        <HomeMoviesListItem />
-        <HomeMoviesListItem />
-        <HomeMoviesListItem />
-        <HomeMoviesListItem />
-        <HomeMoviesListItem />
-        <HomeMoviesListItem />
-        <HomeMoviesListItem />
-        <HomeMoviesListItem />
-        <HomeMoviesListItem />
+        {movies.map((movie, index) => (
+          <HomeMoviesListItem movie={movie} key={index} />
+        ))}
       </div>
     </section>
   );
 };
 export default Popular;
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await axios.get(
+    process.env.NEXT_PUBLIC_API_LINK + "/tmdb/popular"
+  );
+
+  // const  topRatedMovies = await fetch(process.env.NEXT_PUBLIC_API_LINK + "/tmdb/topRated")
+  // const data = await topRatedMovies.json()
+
+  // Pass data to the page via props
+  return {
+    props: { movies: res.data },
+  };
+}
