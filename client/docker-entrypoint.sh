dc381763ca4948f8baf4f910b0140f0ddaa8e081
ILYAS IMZAGNAN <imzagnan.ii@gmail.com>
2022-06-24 01:29:05 +0100

    improve docker config

--- client/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Waiting for Backend Server to start..."
-./wait-for backend:8080
-
-echo "Starting the Frontend Server..."
-yarn start
--- server/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Waiting for MySQL to start..."
-./wait-for db:3306
-
-echo "Starting the server..."
-java -jar app.jar
